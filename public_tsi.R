e0314806d25a8035ea925254bc51aed2ba5432be
Regina Lionheart <38685718+R-Lionheart@users.noreply.github.com>
2025-09-17 12:33:40 -0700

    Move recent public TSI data to separate folder. Addresses issue #75 in streams repo.

--- public_tsi.R
+++ public_tsi.R
@@ -1,32 +1,12 @@
 library(tidyverse)
 
+## This is a modified TSI calculation that includes the most recent growing 
+## period (June to October) for the TSI values; at least 3 of the 6 months of data.
+
+
 lakes_wq_dat<-readRDS('outputs/lakes_wq_dat.RDS')
 
-tsi_calc<-function(data,epi_depth=5,startMonth=6,endMonth=10){
-  data |>
-    dplyr::filter(parameter %in% c('Secchi Depth','Water transparency','Total Phosphorus','Chlorophyll a')&
-                    Month>=startMonth&Month<=endMonth&
-                    depth<=epi_depth) |>
-    dplyr::group_by(Year,parameter) |>
-    dplyr::summarise(SummerMean=mean(newResultValue,na.rm=T),.groups='drop') |>
-    dplyr::ungroup() |>
-    dplyr::mutate(TSI=round(ifelse(
-      parameter=='Total Phosphorus', 14.42*log(SummerMean*1000) +4.15,
-      ifelse(parameter=='Chlorophyll a', 9.81*log(SummerMean) + 30.6,
-             ifelse( parameter=='Secchi Depth'|parameter=='Water transparency' , 60- 14.41*log(SummerMean),
-                     NA))), 1)
-    )
-}
-
-lakes_tsi_data <- lakes_wq_dat |>
-  group_by(SITE_CODE)|>
-  tidyr::nest()|>
-  mutate(TSI_out=purrr::map(.x=data,.f=~tsi_calc(.x)))|>
-  select(-data)|>
-  tidyr::unnest(TSI_out)
-
-## modifications ---------------------------------------------
-tsi_calc2 <-function(data,epi_depth=5,startMonth=6,endMonth=10){
+tsi_calc_recent <-function(data,epi_depth=5,startMonth=6,endMonth=10){
   data |>
     dplyr::filter(parameter %in% c('Secchi Depth','Water transparency','Total Phosphorus','Chlorophyll a')&
                     Month>=startMonth&Month<=endMonth&
@@ -43,9 +23,11 @@ tsi_calc2 <-function(data,epi_depth=5,startMonth=6,endMonth=10){
     )
   }
 
-lakes_tsi_data2 <- lakes_wq_dat |>
+lakes_tsi_data_recent <- lakes_wq_dat |>
   group_by(SITE_CODE)|>
   tidyr::nest()|>
-  mutate(TSI_out=purrr::map(.x=data,.f=~tsi_calc2(.x)))|>
+  mutate(TSI_out=purrr::map(.x=data,.f=~tsi_calc_recent(.x)))|>
   select(-data)|>
   tidyr::unnest(TSI_out)
+
+write_csv(lakes_tsi_data_recent, "public_dashboard_outputs/lakes_tsi_data_recent.csv")
