f01a256cb84c12a2beb2d4e89a67a8747c6cfa09
Regina Lionheart <38685718+R-Lionheart@users.noreply.github.com>
2025-11-10 16:42:42 -0800

    Remove all cached files in gitignore.

--- app.R
+++
@@ -1,8 +0,0 @@
-library(shiny)
-source("ui.R")
-if (!file.exists("server.R")) {
-  server <- function(input, output, session) { }
-} else {
-  source("server.R") # defines server
-}
-shinyApp(ui = ui, server = server)
